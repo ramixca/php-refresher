eaae8d2a1a7bc8c1baa8168ef09122701ffefe49
ramixca <70819559+ramixca@users.noreply.github.com>
2024-09-09 23:11:42 -0700

    function php solved

--- solution.php
+++ solution.php
@@ -17,4 +17,26 @@ print("QTY: 1 Milkshake PRICE: $milkshake\n");
 print("QTY: 1 Cola PRICE: $cola\n");
 print("Subtotal: $subtotal \n");
 print("Total with tax: " . number_format($total, 2) . "\n");
+
+//Part 2. Print out a string containing first and last name separated by a space. Also print out the length of that string.
+
+$first_name = "Ramon";
+$last_name = "Gonzalo";
+$full_name = $first_name . " " . $last_name;
+print("Full Name: $full_name\n");
+print("Length of Full Name: " . strlen($full_name) . "\n");
+
+//use the increment operator and the combines multiplication operator to print out the numbers from 1 to 5 and powers of 2 from 2 to 32.
+
+
+function incrementCount() {
+    $numbers = 1;
+    for ($i = 1; $i <= 5; $i++) {
+        print("Number: $numbers\n");
+        $numbers *= 2;
+    }
+}
+
+incrementCount();
+
 ?>
